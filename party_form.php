3b2d86032f1ff03405a7db5cabd6aed3d6df17e9
vladimirbroucek <broukla987@gmail.com>
2025-04-04 19:45:24 -0700

    fix html

--- party_form.php
+++ party_form.php
@@ -1,38 +1,41 @@
 <?php
+$items = [
+    "Cake", "Balloons", "Music System", "Lights", "Catering Service",
+    "DJ", "Photo Booth", "Tables", "Chairs", "Drinks",
+    "Party Hats", "Streamers", "Invitation Cards", "Party Games", "Cleaning Service"
+];
+
+$output = "";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['items'])) {
     $selected = $_POST['items'];
     $indices = implode(",", $selected);
 
-    $command = escapeshellcmd("python3 party_planner.py " . escapeshellarg($indices));
+    $command = escapeshellcmd("python3 /var/www/html/party_planner.py " . escapeshellarg($indices));
     $output = shell_exec($command);
-
-    echo $output;
-    exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Party Planner Form</title>
+    <title>Party Planner</title>
 </head>
 <body>
 <h1>Select Party Items</h1>
-<form method="POST" action="">
-    <?php
-    $items = [
-        "Cake", "Balloons", "Music System", "Lights", "Catering Service",
-        "DJ", "Photo Booth", "Tables", "Chairs", "Drinks",
-        "Party Hats", "Streamers", "Invitation Cards", "Party Games", "Cleaning Service"
-    ];
-
-    foreach ($items as $index => $item) {
-        echo "<input type='checkbox' name='items[]' value='$index' id='item$index'>";
-        echo "<label for='item$index'>$item</label><br>";
-    }
-    ?>
+<form method="POST">
+    <?php foreach ($items as $index => $item): ?>
+        <input type="checkbox" name="items[]" value="<?= $index ?>" id="item<?= $index ?>">
+        <label for="item<?= $index ?>"><?= $item ?></label><br>
+    <?php endforeach; ?>
     <br>
     <input type="submit" value="Plan the Party">
 </form>
+
+<?php if (!empty($output)): ?>
+    <hr>
+    <h2>Result:</h2>
+    <?= $output ?>
+<?php endif; ?>
 </body>
 </html>
